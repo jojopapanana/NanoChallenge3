55acec1c3e357623d1e876a2eef07ab221888f91
Hans Jonathan Zebua <hans.zebua@icloud.com>
2024-07-14 07:48:07 +0700

    Adding carousell, input method selection,  Input instructions

--- NanoChallenge3/View/ChooseInputView.swift
+++ NanoChallenge3/View/ChooseInputView.swift
@@ -9,7 +9,61 @@ import SwiftUI
 
 struct ChooseInputView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            VStack(alignment: .center, spacing: 20) {
+                VStack(spacing: 20) {
+                    HStack(spacing: 20) {
+                                    Image(systemName: "camera.fill")
+                                        .resizable()
+                                        .frame(width: 88, height: 66)
+                                        .foregroundColor(.white)
+                                    Text("Scan")
+                                        .font(
+                                            Font.custom("SF Pro", size: 39)
+                                                .weight(.bold)
+                                        )
+                                        .multilineTextAlignment(.center)
+                                        .foregroundColor(Color(red: 1, green: 1, blue: 1))
+                                }
+                        }
+                        .frame(width: 361, height: 250)
+                        .background(Color(red: 1, green: 0.22, blue: 0.36))
+                        .cornerRadius(20)
+                
+                Text("or")
+                    .font(Font.custom("SF Pro", size: 32))
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(Color(red: 0.24, green: 0.24, blue: 0.26))
+                    .opacity(0.6)
+                
+                VStack(spacing: 20) {
+                    HStack(spacing: 20) {
+                                    Image(systemName: "pencil.and.list.clipboard.rtl")
+                                        .resizable()
+                                        .frame(width: 62, height: 66)
+                                        .foregroundColor(Color(red: 1, green: 0.22, blue: 0.36))
+                                    Text("Manual")
+                                    .font(
+                                    Font.custom("SF Pro", size: 39)
+                                    .weight(.bold)
+                                    )
+                                    .multilineTextAlignment(.center)
+                                    .foregroundColor(Color(red: 1, green: 0.22, blue: 0.36))
+                                }
+                        }
+                        .frame(width: 361, height: 250)
+                        .cornerRadius(20)
+                        .overlay(
+                        RoundedRectangle(cornerRadius: 20)
+                        .inset(by: 0.5)
+                        .stroke(Color(red: 1, green: 0.22, blue: 0.36), lineWidth: 1)
+                        )
+            
+            }
+            .padding(0)
+            .frame(width: 361, alignment: .center)
+            .navigationTitle("Input Recipe")
+        }
     }
 }
 
---
+++ NanoChallenge3/View/ScanInstructions.swift
@@ -0,0 +1,23 @@
+//
+//  ScanInstructions.swift
+//  NanoChallenge3
+//
+//  Created by Hans Zebua on 14/07/24.
+//
+
+import SwiftUI
+
+struct ScanInstructions: View {
+    var body: some View {
+        NavigationStack{
+            VStack{
+               Text("Instructions")
+            }
+            .navigationTitle("Instructions")
+        }
+    }
+}
+
+#Preview {
+    ScanInstructions()
+}
