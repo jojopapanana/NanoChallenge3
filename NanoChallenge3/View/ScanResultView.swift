d5f6bb3a08fdef56ffbbbe6e6e3fbe3d1ee06b1e
Hans Jonathan Zebua <hans.zebua@icloud.com>
2024-07-16 22:38:18 +0700

    sukses

--- NanoChallenge3/View/ScanResultView.swift
+++ NanoChallenge3/View/ScanResultView.swift
@@ -3,30 +3,30 @@ import SwiftData
 import Vision
 
 struct ScanResultView: View {
-    @Environment(\.modelContext) private var modelContext
-    @State private var imageAttributes: ImageAttribute?
+@Environment(\.modelContext) private var modelContext
+@State private var imageAttributes: ImageAttribute?
 
-    @StateObject private var recognizeImage = recognizeText()
+@StateObject private var recognizeImage = recognizeText()
 
-    var body: some View {
-        VStack {
-            if let imageData = imageAttributes?.image,
-               let uiImage = UIImage(data: imageData) {
-                Image(uiImage: uiImage)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(height: UIScreen.main.bounds.height * 0.45)
-            } else {
-                Text("No image available")
-            }
+var body: some View {
+    VStack {
+        if let imageData = imageAttributes?.image,
+           let uiImage = UIImage(data: imageData) {
+            Image(uiImage: uiImage)
+                .resizable()
+                .scaledToFit()
+                .frame(height: UIScreen.main.bounds.height * 0.45)
+        } else {
+            Text("No image available")
+        }
+        
+        Button("Recognize") {
+            recognizeImage.modelContext = modelContext
+            recognizeImage.imageAttributes = imageAttributes
+            recognizeImage.recognizeText()
+        }
+  
             
-            Button("Recognize") {
-                recognizeImage.modelContext = modelContext
-                recognizeImage.imageAttributes = imageAttributes
-                recognizeImage.recognizeText()
-            }
-      
-                
 //                VStack {
 //                    Text("Full")
 //                    List {
@@ -57,20 +57,8 @@ struct ScanResultView: View {
 //                        }
 //                    }
 //                }
-            }
-            .padding()
-        }
-        .onAppear {
-            loadImageAttribute()
         }
         .padding()
     }
-    
-    private func loadImageAttribute() {
-        // Fetch the most recent ImageAttribute
-        let fetchRequest = FetchDescriptor<ImageAttribute>()
-        if let fetchedAttributes = try? modelContext.fetch(fetchRequest) {
-            imageAttributes = fetchedAttributes.last
-        }
-    }
 }
+
