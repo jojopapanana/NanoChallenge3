125be69b388eda69d1a3a914a4716ccf95108fa0
Hans Jonathan Zebua <hans.zebua@icloud.com>
2024-07-25 23:34:49 +0700

    add search to view all, not localizable yet

--- NanoChallenge3/View/AllRecipeView.swift
+++ NanoChallenge3/View/AllRecipeView.swift
@@ -9,24 +9,30 @@ import SwiftUI
 import SwiftData
 
 struct AllRecipeView: View {
-    @Query private var recipes:[Recipe]
-    @Binding var navigationPath:NavigationPath
-    
+    @Query private var recipes: [Recipe]
+    @Binding var navigationPath: NavigationPath
+    @State private var searchTerm = ""
+
+    var filteredRecipe: [Recipe] {
+        guard !searchTerm.isEmpty else { return recipes }
+        return recipes.filter { $0.menuName.localizedCaseInsensitiveContains(searchTerm) }
+    }
+
     var body: some View {
-        ScrollView{
+        ScrollView {
             if recipes.isEmpty {
                 recipeCardEmpty()
             } else {
-                VStack(spacing: 18){
-                    ForEach(0..<recipes.count / 2, id: \.self) { rowIndex in
+                VStack(spacing: 18) {
+                    ForEach(0..<filteredRecipe.count / 2, id: \.self) { rowIndex in
                         HStack(spacing: -4) {
                             ForEach(0..<2, id: \.self) { columnIndex in
                                 let cardIndex = rowIndex * 2 + columnIndex
-                                if cardIndex < recipes.count {
-                                    NavigationLink{
-                                        RecipeDetailView(recipe: recipes[recipes.count - cardIndex - 1], navigationPath: $navigationPath)
+                                if cardIndex < filteredRecipe.count {
+                                    NavigationLink {
+                                        RecipeDetailView(recipe: filteredRecipe[filteredRecipe.count - cardIndex - 1], navigationPath: $navigationPath)
                                     } label: {
-                                        RecipeCardView(recipe: recipes[recipes.count - cardIndex - 1])
+                                        RecipeCardView(recipe: filteredRecipe[filteredRecipe.count - cardIndex - 1])
                                     }
                                 }
                             }
@@ -36,9 +42,11 @@ struct AllRecipeView: View {
                 .padding(.top, 20)
             }
         }
-    }
+        .searchable(text: $searchTerm, placement: .navigationBarDrawer(displayMode: .always), prompt: "Search Menu")}
 }
 
 //#Preview {
 //    AllRecipeView()
 //}
+
+
